1e28825f20f1662ba20e9510cabb15ff3df13f30
giteshk11 <writemail2gitesh@gmail.com>
2023-05-20 16:25:30 +0530

    Chore: Added firebase for auth

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
@@ -12,4 +13,6 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig)
 
-export default firebaseApp
+const firebaseAuth = getAuth(firebaseApp)
+
+export { firebaseApp, firebaseAuth }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,40 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheButton from '@/components/TheButton.vue'
+
+import { firebaseAuth } from '@/firebase'
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+
+const provider = new GoogleAuthProvider()
+
+const signupWithGoogle = () => {
+  signInWithPopup(firebaseAuth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result)
+      const token = credential?.accessToken
+      // The signed-in user info.
+      const user = result.user
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+      console.log(result)
+    })
+    .catch((error) => {
+      console.log(error)
+      // Handle Errors here.
+      const errorCode = error.code
+      const errorMessage = error.message
+      // The email of the user's account used.
+      const email = error.customData.email
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error)
+
+      // ...
+    })
+}
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <TheButton @click="signupWithGoogle"> Sign Up with google </TheButton>
+  </main>
 </template>
